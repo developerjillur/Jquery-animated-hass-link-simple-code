8c9e3b97f842fdb4421b3b4907a3f0905d0c28d5
Jillur Rahman <developerjillur@gmail.com>
2016-10-03 08:44:43 +0600

    Create animated-script.js

---
+++ animated-script.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function(){
+	$('menu ul li > a').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
+
